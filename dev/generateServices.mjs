7aa86094c9dd5afc293ede63858a45680322304a
dutu <dutu@protonmail.com>
2026-09-15 23:58:39 +0300

    Update gRPC service generator for split proto definitions
    Signed-off-by: dutu <dutu@protonmail.com>

--- dev/generateServices.mjs
+++ dev/generateServices.mjs
@@ -1,42 +1,45 @@
-import fs from 'fs/promises';
-import protobuf from 'protobufjs';
+import fs from 'fs/promises'
+import protobuf from 'protobufjs'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
-const PROTO_PATH = path.join(__dirname, '../proto/grpc.proto')
+const PROTO_PATH = path.join(__dirname, '../proto/grpc_services.proto')
 
-async function generateCodeFromRoot() {
-  const root = await protobuf.load(PROTO_PATH);
-
-  let output = '';
-  traverse(root, (name, methods) => {
-    output += `${lowerFirstChar(name)} = {\n`;
-    for (const methodName of methods) {
-      output += `  ${lowerFirstChar(methodName)}: (parameters) => {\n`;
-      output += `    return this.#callRPCMethod('${name}', '${methodName}', parameters)\n`;
-      output += `  },\n`;
-    }
-    output += '}\n\n';
-  });
-
-  await fs.writeFile('services.txt', output);
-  console.log('The file has been saved!');
+const lowerFirstChar = function(str) {
+  return str.charAt(0).toLowerCase() + str.slice(1)
 }
 
-function traverse(node, onService) {
+const traverse = function(node, onService) {
   for (const [name, value] of Object.entries(node.nested || {})) {
     if (value instanceof protobuf.Service) {
-      const methods = Object.keys(value.methods);
-      onService(name, methods);
+      const methods = Object.keys(value.methods)
+      onService(name, methods)
     } else if (value instanceof protobuf.Namespace) {
-      traverse(value, onService);
+      traverse(value, onService)
     }
   }
 }
 
-function lowerFirstChar(str) {
-  return str.charAt(0).toLowerCase() + str.slice(1);
+const generateCodeFromRoot = async function() {
+  const root = await protobuf.load(PROTO_PATH)
+
+  let output = ''
+
+  traverse(root, (name, methods) => {
+    output += `${lowerFirstChar(name)} = {\n`
+
+    for (const methodName of methods) {
+      output += `  ${lowerFirstChar(methodName)}: (parameters) => {\n`
+      output += `    return this.#callRPCMethod('${name}', '${methodName}', parameters)\n`
+      output += `  },\n`
+    }
+
+    output += '}\n\n'
+  })
+
+  await fs.writeFile('services.txt', output)
+  console.log('The file has been saved!')
 }
 
-generateCodeFromRoot().catch(console.error);
+generateCodeFromRoot().catch(console.error)
